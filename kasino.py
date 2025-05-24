8910eb77b4ccb7aa2a99d6f17c3bc8bd9cb246d7
chelik10228 <dxxeas@gmail.com>
2025-05-24 16:16:46 +0200

    first commit

---
+++ kasino.py
@@ -0,0 +1,8 @@
+import random;
+import time;
+
+dep = input("Введи что ты хочешь депнуть в казик: ");
+print(f"Ты депнул {dep} в казик");
+time.sleep(2);
+deplist = [f"Ты проиграл {dep} в казике :(", "Ты выйграл 2 {dep} в казике! :)"];
+print(random.choice(deplist));
